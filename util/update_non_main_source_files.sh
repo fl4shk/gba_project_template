39aff2055a990ea69a63fba5d2e10b0cfbb49abc
fl4shk <fl4shk@users.noreply.github.com>
2016-07-29 16:26:38 -0500

    More complete script

--- util/update_non_main_source_files.sh
+++ util/update_non_main_source_files.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-rsync -avvvr --dry-run --existing "$1"/src `pwd`/src 
+
+#rsync -avh --progress --dry-run --existing "$1"/src .
+#rsync -avh --progress --dry-run --existing "$1"/src .
+rsync -avuh --progress --exclude=".git" --exclude="src/main.thumb.cpp" \
+	--exclude=".*.swp" --exclude=".*.swo" --existing "$1"/ .
+
+
+find src -type f -exec sed -i 's/Sherwin'\''s Adventure/GBA Project Template/g' {} +
+
+
