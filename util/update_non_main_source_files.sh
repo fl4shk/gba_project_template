c02c29d819bec50d59245618f527656830de56ec
fl4shk <fl4shk@users.noreply.github.com>
2017-02-05 13:43:43 -0500

    (slightly) update util scripts

--- util/update_non_main_source_files.sh
+++ util/update_non_main_source_files.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# This file is part of GBA Project Template.
+# 
+# Copyright 2015-2017 by Andrew Clark (FL4SHK).
+# 
+# GBA Project Template is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+# 
+# GBA Project Template is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with GBA Project Template.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#!/bin/bash
+
 first_script=util/get_most_rsync_exclude_args.sh
 second_script=util/convert_project_name_in_license.sh
 
